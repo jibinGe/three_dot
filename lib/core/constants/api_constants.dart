eb86b78b31c707a78c3a557b5c80711aa6e37140
aslam-nexonetics <aslamm@nexonetics.com>
2024-11-28 17:05:28 +0530

    api updated

--- lib/core/constants/api_constants.dart
+++ lib/core/constants/api_constants.dart
@@ -6,8 +6,8 @@ class ApiConstants {
       return 'http://52.76.183.19:8001'; // For web
     } else {
       if (!kReleaseMode) {
+        // return 'http://192.168.29.61:8000'; // For Android emulator
         return 'http://52.76.183.19:8001'; // For Android emulator
-        // return 'http://localhost:8000'; // For iOS simulator
       } else {
         return 'http://52.76.183.19:8001'; // For production
       }
