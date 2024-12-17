18eb1058506385efb021d500d419c5f6b021ad6e
aslam-nexonetics <aslamm@nexonetics.com>
2024-12-17 12:50:44 +0530

    api base url updated

--- lib/core/constants/api_constants.dart
+++ lib/core/constants/api_constants.dart
@@ -1,16 +1,17 @@
 import 'package:flutter/foundation.dart';
 
 class ApiConstants {
-  static String get baseUrl {
-    if (kIsWeb) {
-      return 'http://52.76.183.19:8001'; // For web
-    } else {
-      if (!kReleaseMode) {
-        // return 'http://192.168.29.61:8000'; // For Android emulator
-        return 'http://52.76.183.19:8001'; // For Android emulator
-      } else {
-        return 'http://52.76.183.19:8001'; // For production
-      }
-    }
-  }
+  // static String get baseUrl {
+  //   if (kIsWeb) {
+  //     return 'http://52.76.183.19:8001'; // For web
+  //   } else {
+  //     if (!kReleaseMode) {
+  //       // return 'http://192.168.29.61:8000'; // For Android emulator
+  //       return 'http://52.76.183.19:8001'; // For Android emulator
+  //     } else {
+  //       return 'http://52.76.183.19:8001'; // For production
+  //     }
+  //   }
+  // }
+  static String get baseUrl => "https://api.3-dot.in";
 }
